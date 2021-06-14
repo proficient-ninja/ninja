4b9da23ecc5e4989a6ee1ab437e16642291f3480
wglee0511 <ranazang@naver.com>
2021-06-14 09:45:32 +0900

    [BOJ, 2588] 곱셈

---
+++ ssammoo/BOJ/BOJ_2588.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+
+b_100 = b // 100
+b_10 = (b - b_100 * 100) // 10
+b_1 = (b - b_100*100 - b_10 * 10)
+
+
+print(a * b_1)
+print(a * b_10)
+print(a * b_100)
+print((b_100*100 + b_10 * 10 + b_1) * a)
