c1f3b7623437df62da627fd82782c025cacf56f6
wglee0511 <ranazang@naver.com>
2021-06-22 20:29:55 +0900

    [BOJ, 1927] 최소힙

---
+++ ssammoo/BOJ/BOJ_1927.py
@@ -0,0 +1,17 @@
+import sys
+import heapq
+
+N = int(input())
+
+heap = []
+
+for each in range(N) :
+    input_val = int(sys.stdin.readline())
+    if input_val == 0 :
+        if len(heap) != 0 :
+            print(heapq.heappop(heap))
+        else :
+            print(0)
+    else :
+        heapq.heappush(heap, input_val)
+
